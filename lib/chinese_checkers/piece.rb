f25593a5660a63ded203f5960f87707a4ec7af6d
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2015-11-21 14:22:32 -0600

    Complete test for piece.

--- lib/chinese_checkers/piece.rb
+++ lib/chinese_checkers/piece.rb
@@ -1,9 +1,11 @@
 module ChineseCheckers
   class Piece
-    attr_reader :x
+    attr_accessor :x, :y, :color
 
-    def initialize(x:, y:)
+    def initialize(x:, y:, color: nil)
      @x = x
+     @y = y
+     @color = color
     end
   end
 end
--- test/chinese_checkers/piece_test.rb
+++ test/chinese_checkers/piece_test.rb
@@ -7,5 +7,33 @@ module ChineseCheckers
       piece = Piece.new(x: 4, y: 7)
       assert_equal 4, piece.x
     end
+
+    def test_it_stores_y_position
+      piece = Piece.new(x: 4, y: 7)
+      assert_equal 7, piece.y
+    end
+
+    def test_it_stores_color
+      piece = Piece.new(x: 4, y: 7, color: :red)
+      assert_equal :red, piece.color
+    end
+
+    def test_it_allows_to_modify_x
+      piece = Piece.new(x: 4, y: 7)
+      piece.x = 5
+      assert_equal 5, piece.x
+    end
+
+    def test_it_allows_to_modify_y
+      piece = Piece.new(x: 4, y: 7)
+      piece.y = 1
+      assert_equal 1, piece.y
+    end
+
+    def test_it_allows_to_modify_color
+      piece = Piece.new(x: 4, y: 7, color: :red)
+      piece.color = :green
+      assert_equal :green, piece.color
+    end
   end
 end
