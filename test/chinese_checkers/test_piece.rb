a1323a5896019a5b24aaa9db9c5f108015d51a34
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2015-11-26 08:42:48 -0600

    Refactor test to make it simpler.

--- test/chinese_checkers/test_piece.rb
+++ test/chinese_checkers/test_piece.rb
@@ -3,36 +3,28 @@ require 'chinese_checkers/piece'
 
 module ChineseCheckers
   class TestPiece < Minitest::Test
-    def test_it_stores_x_position
-      piece = Piece.new(x: 4, y: 7)
-      assert_equal 4, piece.x
-    end
-
-    def test_it_stores_y_position
-      piece = Piece.new(x: 4, y: 7)
-      assert_equal 7, piece.y
-    end
-
-    def test_it_stores_color
+    def test_it_stores_x_y_and_color
       piece = Piece.new(x: 4, y: 7, color: :red)
+
+      assert_equal 4, piece.x
+      assert_equal 7, piece.y
       assert_equal :red, piece.color
     end
 
-    def test_it_allows_to_modify_x
+    def test_it_can_be_created_with_no_color
       piece = Piece.new(x: 4, y: 7)
-      piece.x = 5
-      assert_equal 5, piece.x
+
+      assert_equal nil, piece.color
     end
 
-    def test_it_allows_to_modify_y
-      piece = Piece.new(x: 4, y: 7)
-      piece.y = 1
-      assert_equal 1, piece.y
-    end
-
-    def test_it_allows_to_modify_color
+    def test_it_allows_to_modify_x_y_and_color
       piece = Piece.new(x: 4, y: 7, color: :red)
+      piece.x = 5
+      piece.y = 1
       piece.color = :green
+
+      assert_equal 5, piece.x
+      assert_equal 1, piece.y
       assert_equal :green, piece.color
     end
   end
